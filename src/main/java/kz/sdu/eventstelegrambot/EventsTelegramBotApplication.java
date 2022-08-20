409eb086dec313c0eac5b3eb714414967c420f37
Sanzhar Zhanibekov <semid46@gmail.com>
2022-08-21 01:18:10 +0600

    init

---
+++ src/main/java/kz/sdu/eventstelegrambot/EventsTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package kz.sdu.eventstelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventsTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/sdu/eventstelegrambot/EventsTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.sdu.eventstelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
